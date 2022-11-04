eb96791cf8bcd56f9a9b2c5a5c8ecd0045744d06
Antoine Degenne <antoine.degenne@agroparistech.fr>
2022-11-04 11:00:02 +0100

    modifs du script star fa"

--- script_star_fa.sh
+++ script_star_fa.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-for i in {1..23}
+for i in {1..22}
 	do 
 	wget "https://ftp.ensembl.org/pub/release-108/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.$i.fa.gz"	
 	done
+wget "https://ftp.ensembl.org/pub/release-108/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.X.fa.gz"
+wget "https://ftp.ensembl.org/pub/release-108/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.chromosome.Y.fa.gz"
 gunzip Homo*
 cat Homo* >> ref.fa
